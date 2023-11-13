4ac7f6d23fc792aea7ba2e7120795484460185bb
Dinko Kovachev <dinko.kovachev.a56@learn.telerikacademy.com>
2023-11-13 16:54:59 +0200

    Added History interface to keep track of the historyChanges in ActivityLogImpl. Finished ActivityClass & Comment Class alongside their interfaces(need to double-check the hierarchy & methods in the ActivityLog Interface with the other classes)

--- taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/models/CommentImpl.java
+++ taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/models/CommentImpl.java
@@ -1,4 +1,32 @@
 package com.company.oop.taskmanagementsytemgroup11.models;
 
-public class CommentImpl {
+import com.company.oop.taskmanagementsytemgroup11.models.contracts.Comment;
+
+public class CommentImpl implements Comment {
+
+    private String content;
+    private String author;
+
+    public CommentImpl(String content, String author) {
+        setContent(content);
+        setAuthor(author);
+    }
+
+    @Override
+    public String getContent() {
+        return content;
+    }
+
+    private void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String getAuthor() {
+        return author;
+    }
+
+    private void setAuthor(String author) {
+        this.author = author;
+    }
 }
--- taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/models/contracts/Comment.java
+++ taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/models/contracts/Comment.java
@@ -1,4 +1,7 @@
 package com.company.oop.taskmanagementsytemgroup11.models.contracts;
 
 public interface Comment {
+
+    String getContent();
+    String getAuthor();
 }
