e3c9e2b4954234eb3fbf96371cf7c278f3cda1f8
Dinko Kovachev <dinko.kovachev.a56@learn.telerikacademy.com>
2023-11-13 12:23:54 +0200

    AddComment update & exceptions update

--- taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/commands/creation/AddComment.java
+++ taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/commands/creation/AddComment.java
@@ -2,5 +2,10 @@ package com.company.oop.taskmanagementsytemgroup11.commands.creation;
 
 import com.company.oop.taskmanagementsytemgroup11.commands.contracts.Command;
 
+import java.util.List;
+
 public class AddComment implements Command {
+
+
+    }
 }
