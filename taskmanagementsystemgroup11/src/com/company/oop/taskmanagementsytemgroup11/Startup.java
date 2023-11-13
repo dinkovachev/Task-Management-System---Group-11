7fab57fb6bbbf4ade94bb785ce7ac52b029be766
Petar <peterkrandev@gmail.com>
2023-11-13 14:55:49 +0200

    commit Startup class

--- taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/Startup.java
+++ taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/Startup.java
@@ -1,4 +1,10 @@
 package com.company.oop.taskmanagementsytemgroup11;
 
+import com.company.oop.taskmanagementsytemgroup11.core.TaskManagementSystemEngineImpl;
+
 public class Startup {
+    public static void main(String[] args) {
+        TaskManagementSystemEngineImpl engine = new TaskManagementSystemEngineImpl();
+        engine.start();
+    }
 }
