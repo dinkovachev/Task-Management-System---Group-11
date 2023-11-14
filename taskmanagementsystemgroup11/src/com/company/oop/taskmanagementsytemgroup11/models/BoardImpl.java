2bf78ff5a5f7d8feaa738e77facd45dc20014ccc
Petar <peterkrandev@gmail.com>
2023-11-14 12:19:41 +0200

    commit  Members, team, board implementations

--- taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/models/BoardImpl.java
+++ taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/models/BoardImpl.java
@@ -1,4 +1,66 @@
 package com.company.oop.taskmanagementsytemgroup11.models;
 
-public class BoardImpl {
+import com.company.oop.taskmanagementsytemgroup11.models.contracts.ActivityLog;
+import com.company.oop.taskmanagementsytemgroup11.models.contracts.Board;
+import com.company.oop.taskmanagementsytemgroup11.models.contracts.Task;
+import com.company.oop.taskmanagementsytemgroup11.utils.ValidationHelpers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardImpl implements Board, ActivityLog {
+
+    private static final int MINIMUM_SYMBOLS = 5;
+    private static final int MAXIMUM_SYMBOLS = 15;
+    public static final String BOARD_NAME_ERR_MSG = String.format(
+            "The Board name's length cannot be less than %d or more than %d symbols long.",
+            MINIMUM_SYMBOLS, MAXIMUM_SYMBOLS);
+    private final List<Board> boards;
+    private String name;
+
+
+
+    public BoardImpl(String name) {
+        setName(name);
+        this.boards = new ArrayList<>();
+    }
+
+    @Override
+    public void addTask(Task task) {
+
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        ValidationHelpers.validateIntRange(name.length(), MINIMUM_SYMBOLS, MAXIMUM_SYMBOLS, BOARD_NAME_ERR_MSG);
+        this.name = name;
+    }
+    //todo validate unique board name
+    @Override
+    public String showTeamActivity() {
+        return null;
+    }
+
+    @Override
+    public String showMemberActivity() {
+        return null;
+    }
+
+    @Override
+    public String showBoardActivity() {
+        return null;
+    }
+
+    public void addBoard(Board boards) {
+        this.boards.add(board);
+    }
+    public List<Board> getBoards() {
+        return new ArrayList<>(boards);
+    }
 }
+
+
--- taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/models/contracts/Board.java
+++ taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/models/contracts/Board.java
@@ -1,4 +1,8 @@
 package com.company.oop.taskmanagementsytemgroup11.models.contracts;
 
 public interface Board {
+
+     void addTask(Task task);
+
+     String getName();
 }
