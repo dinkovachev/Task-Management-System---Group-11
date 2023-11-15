542810ab94d3cc8142631f510729c761c57eeec0
Petar <peterkrandev@gmail.com>
2023-11-15 10:43:13 +0200

    commit shall all members, teams, boards commands

---
+++ taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/commands/creation/ShowAllTeamsBoardsCommand.java
@@ -0,0 +1,24 @@
+package com.company.oop.taskmanagementsytemgroup11.commands.creation;
+
+
+import com.company.oop.taskmanagementsytemgroup11.core.contracts.TaskManagementSystemRepository;
+import com.company.oop.taskmanagementsytemgroup11.models.contracts.Board;
+import com.company.oop.taskmanagementsytemgroup11.utils.ListingHelpers;
+
+import java.util.List;
+
+public class ShowAllTeamsBoardsCommand extends BaseCommand {
+    private final List<Board> boards;
+
+    public ShowAllTeamsBoardsCommand(TaskManagementSystemRepository taskManagementSystemRepository) {
+        super(taskManagementSystemRepository);
+        boards = taskManagementSystemRepository.getAllTeamsBoards();
+    }
+    @Override
+    protected String executeCommand(List<String> parameters) {
+        if (boards.isEmpty()) {
+            return "There are no registered boards.";
+        }
+        return ListingHelpers.boardToString(boards);
+    }
+}
--- taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/commands/creation/ShowAllTeamsCommand.java
+++ taskmanagementsystemgroup11/src/com/company/oop/taskmanagementsytemgroup11/commands/creation/ShowAllTeamsCommand.java
@@ -1,4 +1,23 @@
 package com.company.oop.taskmanagementsytemgroup11.commands.creation;
 
+import com.company.oop.taskmanagementsytemgroup11.core.contracts.TaskManagementSystemRepository;
+import com.company.oop.taskmanagementsytemgroup11.models.contracts.Team;
+import com.company.oop.taskmanagementsytemgroup11.utils.ListingHelpers;
+
+import java.util.List;
+
 public class ShowAllTeamsCommand extends BaseCommand {
+    private final List<Team> teams;
+
+    public ShowAllTeamsCommand(TaskManagementSystemRepository taskManagementSystemRepository) {
+        super(taskManagementSystemRepository);
+        teams = taskManagementSystemRepository.getAllTeams();
+    }
+    @Override
+    protected String executeCommand(List<String> parameters) {
+        if (teams.isEmpty()) {
+            return "There are no registered teams.";
+        }
+        return ListingHelpers.teamsToString(teams);
+    }
 }
