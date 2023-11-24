394cafbc8884d397ff90f6538a7aa5aa720c2a61
Petar <peterkrandev@gmail.com>
2023-11-24 10:35:31 +0200

    commit fixed bug in bug :)

--- test/com/company/oop/taskmanagementsystemgroup11/tests/models/BoardImplTest.java
+++ test/com/company/oop/taskmanagementsystemgroup11/tests/models/BoardImplTest.java
@@ -1,4 +1,9 @@
 package com.company.oop.taskmanagementsystemgroup11.tests.models;
 
+import com.company.oop.taskmanagementsytemgroup11.models.BoardImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class BoardImplTest {
+
 }
